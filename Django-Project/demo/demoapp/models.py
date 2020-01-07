045b0b79d21978b9dcb6f0585bab178dd5939b61
AjayKarki <ajaykarki333@gmail.com>
2020-01-07 08:54:41 +0545

    Models

--- Django-Project/demo/demoapp/models.py
+++ Django-Project/demo/demoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    roll = models.IntegerField()
+    section = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
--- Django-Project/demo/demoapp/views.py
+++ Django-Project/demo/demoapp/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render,HttpResponse
 
 def contact_us(request):
     return render(request,'contactus.html')
+
+
